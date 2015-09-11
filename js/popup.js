bc3a79d217b01eb288a804b86f298970f3146ada
Paul Fortin <paul@cp-soft.com>
2015-09-11 07:08:17 -0400

    Save last inputs

--- js/popup.js
+++ js/popup.js
@@ -1,8 +1,8 @@
 var myApp = angular.module('myApp',[]);
 
 myApp.controller('myCtrl', ['$scope', function($scope) {
-	$scope.hours = 0;
-	$scope.mins = 0;
+	$scope.hours = localStorage["rp_calc-hours"];
+	$scope.mins = localStorage["rp_calc-mins"];
 	
 	$scope.rp_time = moment(new Date()).add('hours', ($scope.hours-1)).add('minutes', $scope.mins).format('MM/DD/YYYY HH:mm:ss') + "\t" + 
         			 moment(new Date()).add('hours', $scope.hours).add('minutes', $scope.mins).format('MM/DD/YYYY HH:mm:ss');
@@ -10,6 +10,8 @@ myApp.controller('myCtrl', ['$scope', function($scope) {
     $scope.calctime = function() {
         $scope.rp_time = moment(new Date()).add('hours', ($scope.hours-1)).add('minutes', $scope.mins).format('MM/DD/YYYY HH:mm:ss') + "\t" + 
         				 moment(new Date()).add('hours', $scope.hours).add('minutes', $scope.mins).format('MM/DD/YYYY HH:mm:ss');
+        localStorage["rp_calc-hours"] = $scope.hours;
+        localStorage["rp_calc-mins"] = $scope.mins;
     };
 }]);
 
